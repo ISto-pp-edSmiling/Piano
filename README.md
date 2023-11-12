b0871749cdf5f2b1c795a119be686b033586b62b
matt <fortnitenoscopes008@gmail.com>
2023-11-12 13:48:35 +0000

    very close just need to add volume slide

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-#FUNNY LOOKIN PIANO
+#Piano
+
+Hello im making a piano, however im using someones web piano for reference.
---
+++ script.js
@@ -0,0 +1,30 @@
+const pianoKeys = document.querySelectorAll(".piano-keys .key"),
+volumeSlider = document.querySelector(".volume-slider input");
+
+let allKeys = [], 
+audio = new Audio("tunes/a.wav"); // by defualt, audio src is "a" tune, however you could just leave this empty
+
+const playTune = (key) => {
+    audio.src = `tunes/${key}.wav`; // passing audio based on key pressed
+    audio.play(); // playing audio
+
+    const clickedKey = document.querySelector(`[data-key="${key}"]`);
+    clickedKey.classList.add("active"); // adding active class to the clicked key element
+    setTimeout(() => { // removing active class after 150ms from the clicked key element
+       clickedKey.classList.remove("active") 
+    }, 120);
+}
+
+pianoKeys.forEach(key => {
+    allKeys.push(key.dataset.key) // adding data-key value to the allKeys array
+    // calling playTune function with passing data-key value as an argument
+    key.addEventListener("click", () => playTune(key.dataset.key));
+});
+
+const pressedKey = (yuh) => {
+    // if the pressed key is in allKeys array, only call the playTune function
+    if (allKeys.includes(yuh.key)) playTune(yuh.key);
+}
+
+document.addEventListener("keydown", pressedKey);
+volumeSlider.addEventListener("input", handleVolume);
