c2c73a9f36b404deabdf9cd51ae8ef2cf0a44d82
matt <fortnitenoscopes008@gmail.com>
2023-11-25 22:20:48 +0000

    donezos

--- script.js
+++ script.js
@@ -12,27 +12,30 @@ const playTune = (key) => {
     const clickedKey = document.querySelector(`[data-key="${key}"]`);
     clickedKey.classList.add("active"); // adding active class to the clicked key element
     setTimeout(() => { // removing active class after 150ms from the clicked key element
-       clickedKey.classList.remove("active") 
+       clickedKey.classList.remove("active")
     }, 120);
 }
 
-pianoKeys.forEach(key => {
-    allKeys.push(key.dataset.key) // adding data-key value to the allKeys array
-    // calling playTune function with passing data-key value as an argument
-    key.addEventListener("click", () => playTune(key.dataset.key));
+pianoKeys.forEach(eachk => {  // forEach is an iterator which looks for every instance of ".piano-keys .key".
+    allKeys.push(eachk.dataset.key) // adding each data-key value to the allKeys array
+    // calling playTune function with passing data-key value as an argument when a key is pressed but not when typed
+    // look at pressedKey for when a keyboard button ias pressed
+    eachk.addEventListener("click", () => playTune(eachk.dataset.key));
 });
 
 const handleVolume = (vol) => {
-    audio.volume = vol.target.value; // passing the range slider valuie as an audio volume
+    audio.volume = vol.target.value; 
+    // passing the range slider value as an audio volume
 }
 
 const showHideKeys = () => {
     pianoKeys.forEach(key => key.classList.toggle("hide"))
 }
 
-const pressedKey = (yuh) => {
-    // if the pressed key is in allKeys array, only call the playTune function
-    if (allKeys.includes(yuh.key)) playTune(yuh.key);
+const pressedKey = (k) => {
+    // if the pressed key is in allKeys array, call the playTune function
+    // this pressedkey thing is only for keyboard use :)
+    if (allKeys.includes(k.key)) playTune(k.key);
 }
 
 keysCheckbox.addEventListener("click", showHideKeys);
